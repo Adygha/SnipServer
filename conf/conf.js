417ea024010d73dfc6564cce17d0101b8d750bd8
Adygha <ja223gs@student.lnu.se>
2017-12-04 20:34:27 +0100

    Just some old playing around.

--- conf/conf.js
+++ conf/conf.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: 4000,
   dbURL: 'mongodb://azmat.se:50000/codeSnipsDB',
-  mainWelcomeMsg: 'Welcome to Code Snippit Server'
+  indexTitle: 'Welcome to Code Snippit Server'
 }
